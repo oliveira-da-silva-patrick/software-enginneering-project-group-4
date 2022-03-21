cb7d324547728efb3db89be8755896f70586732c
vitellaro-matteo <matteo.vitellaro.002@student.uni.lu>
2022-03-21 21:53:47 +0100

    worked on tiles & animation

--- Prototypes/Matteo/Mobile_test_final/Mobile_test/Assets/Scripts/Player.cs
+++ Prototypes/Matteo/Mobile_test_final/Mobile_test/Assets/Scripts/Player.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Joystick joystick;
-    public float speed = 2.0f;
-    public Rigidbody2D rb;
+    public float speed = 1.0f;
+    private Rigidbody2D rb;
     Vector2 movement;
-    public Transform closestEnemy;
-    public bool enemyContact = false;
-    private GameObject[] multipleEnemies;
-   
+
+    bool movingUp;
+    bool movingDown;
+    bool movingSide;
+    Animator animator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
@@ -22,32 +29,5 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
-        closestEnemy = getClosestEnemy();
-        if(closestEnemy != null)
-        {
-            //closestEnemy.gameObject.GetComponent<SpriteRenderer>().material.color = new Color(1, 0.7f, 0, 1);
-        }
-    }
-
-    public Transform getClosestEnemy()
-    {
-        multipleEnemies = GameObject.FindGameObjectsWithTag("Enemy");
-        float closestDistance = Mathf.Infinity;
-        Transform trans = null;
-        foreach(GameObject go in multipleEnemies)
-        {
-            float currentDistance = Vector3.Distance(transform.position, go.transform.position);
-            if(currentDistance < closestDistance && currentDistance < 5)
-            {
-                closestDistance = currentDistance;
-                trans = go.transform;
-                enemyContact = true;
-                go.GetComponent<SpriteRenderer>().material.color = new Color(1, 0.7f, 0, 1);
-            } else
-            {
-                go.GetComponent<SpriteRenderer>().material.color = new Color(0, 0.2f, 1, 1);
-            }
-        }
-        return trans;
     }
 }
