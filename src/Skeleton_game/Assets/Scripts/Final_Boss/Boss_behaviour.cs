090b8e6779ff90c9758cf75d69bae0b8a4f2121f
Daniel Sousa <71495190+sousa-daniel@users.noreply.github.com>
2022-05-24 18:55:27 +0200

    Created Boss attacks

--- src/Skeleton_game/Assets/Scripts/Final_Boss/Boss_behaviour.cs
+++ src/Skeleton_game/Assets/Scripts/Final_Boss/Boss_behaviour.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Boss_behaviour : MonoBehaviour
 {
+
+    // Parameters for FOV (defines whether boss can see and attack player using CircleCast)
     public float radius = 100f;
     public float stoppingDistance = 3f;
     public float retreatDistance = 2f;
@@ -21,18 +23,36 @@ public class Boss_behaviour : MonoBehaviour
 
 
 
-
+    // Calculates distance from player (used in animations)
     private GameObject player;
     private float distanceFromPlayerX;
     private float distanceFromPlayerY;
 
+    //Animator of the Final Boss (could also use method GetComponent but it's irrelevant since this is used in a prefab that doesnt require any setup)
     public Animator animator;
 
+    //Projectile to be thrown by the final boss
     public GameObject projectile;
-    private float timeBtwShots;
-    private float startTimeBtwShots = 0.5f;
 
-    private int rd = 2;
+    // ATTACKS
+    private int rd = 2; //If more attacks are implemented either increase this manually or through some trigger such as health.
+    private float timeBtwShots;
+
+    // Attack 1
+    private float startTimeBtwShotsA1 = 0.5f;
+    private bool attack1 = false;
+    private int a1Counter = 0;
+
+    //Attack 2
+    private float startTimeBtwShotsA2 = 0.2f;
+    private bool attack2 = false;
+    private int a2Counter = 0;
+    
+    // Attack caller
+    private float attackloop;
+    private int startAttackloop = 5;
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -41,15 +61,17 @@ public class Boss_behaviour : MonoBehaviour
     }
 
 
-    void chooseAttack() {
+    void chooseAttack(int type) {
 
-        switch (rd)
+        switch (type)
         {
             case 1:
-                // Attack 1
+                attack1 = true;
+                a1Counter = 0;
                 break;
             case 2:
-                // Attack 2
+                attack2 = true;
+                a2Counter = 0;
                 break;
             case 3:
                 // Attack 3
@@ -64,7 +86,8 @@ public class Boss_behaviour : MonoBehaviour
 
 
 
-
+    // Verifies whether Final Boss is dead or not. Currently Boss is considered dead at 150 health to avoid being destroyed by methods in 'healthSystem'
+    // script component before the Death animation can actually play.
     bool checkDeath() {
         if (gameObject.GetComponent<healthSystem>().health <= 100)
         {
@@ -72,63 +95,118 @@ public class Boss_behaviour : MonoBehaviour
         }else return false;
     }
 
+    // This method starts the Boss. It is called in an animation event, namely in the 'Boss_Awaken' animation.
     void startBoss() {
         animator.SetBool("startFight", true);
     }
 
+    // Each 
+    void attackCallerTimer() {
+
+        if (attackloop <= 0)
+        {
+            var x = Random.Range(1,rd+1);
+            chooseAttack(x);
+            attackloop = startAttackloop;
+            Debug.Log("Attack " + x + " activated.");
+
+        } else
+        {
+            attackloop -= Time.deltaTime; 
+        }
+    }
 
 
     // Update is called once per frame
     void Update()
     {
+        // Tells Animator if Boss is dead.
         animator.SetBool("isDead", checkDeath());
 
         if (animator.GetBool("startFight"))
         {
+            // Movement
             FOV();
+
+            // Attacks
+
+            //This method activates 1 of the attacks randomly
+            attackCallerTimer();
+
+            //Checks which attacks are activated and calls them
+            //Attack 1
+            if (attack1 && a1Counter <= 8)
+            {
+                spamProjectiles();
+            } else {
+                attack1 = false;
+            }
+
+            // Attack 2
+            if (attack2 && a2Counter <= 5)
+            {
+                spamFourDirections();
+            } else {
+                attack2 = false;
+            }
         }
         
+        // Sends horizontal and vertical values of the Boss relative to the player, this is used to determine which animation should play
         distanceFromPlayerX = player.transform.position.x - transform.position.x;
         distanceFromPlayerY = player.transform.position.y - transform.position.y;
         animator.SetFloat("Horizontal", distanceFromPlayerX);
         animator.SetFloat("Vertical", distanceFromPlayerY);
         
-
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            spamProjectiles();
-        }
-        spamProjectiles();
     }
+
+    // ATTACK METHODS
 
     void spamProjectiles() {
 
         if (timeBtwShots <= 0)
         {
-            Instantiate(projectile, transform.position, Quaternion.identity);
-            timeBtwShots = startTimeBtwShots;
+            //Instantiates projectiles and sends each in random direction and speed
+
+            var x = Random.Range(-2f, 2f); 
+            var y = Random.Range(-2f, 2f);
+            
+            GameObject ProjectileObject = (GameObject)Instantiate(projectile, transform.position, Quaternion.identity);
+
+            ProjectileObject.GetComponent<Rigidbody2D>().velocity = new Vector2(x*10,y*10);
+
+            timeBtwShots = startTimeBtwShotsA1;
+            a1Counter++;
         } else
         {
             timeBtwShots -= Time.deltaTime; 
         }
-
-        // for (int i = 0; i < 10; i++)
-        // {
-
-        //     Instantiate(projectile, transform.position, Quaternion.identity);
-        // }
     }
 
+    void spamFourDirections() {
+        if (timeBtwShots <= 0)
+        {
 
+            //Instantiates and lauches projectiles in 4 directions in a spiral pattern
+            
+            GameObject ProjectileObject1 = (GameObject)Instantiate(projectile, transform.position, Quaternion.identity);
+            GameObject ProjectileObject2 = (GameObject)Instantiate(projectile, transform.position, Quaternion.identity);
+            GameObject ProjectileObject3 = (GameObject)Instantiate(projectile, transform.position, Quaternion.identity);
+            GameObject ProjectileObject4 = (GameObject)Instantiate(projectile, transform.position, Quaternion.identity);
 
+            ProjectileObject1.GetComponent<Rigidbody2D>().velocity = new Vector2(5,0 + a2Counter);
+            ProjectileObject2.GetComponent<Rigidbody2D>().velocity = new Vector2(-5,0 - a2Counter);
+            ProjectileObject3.GetComponent<Rigidbody2D>().velocity = new Vector2(0 - a2Counter,5);
+            ProjectileObject4.GetComponent<Rigidbody2D>().velocity = new Vector2(0 + a2Counter,-5);
 
+            timeBtwShots = startTimeBtwShotsA2;
+            a2Counter++;
+        } else
+        {
+            timeBtwShots -= Time.deltaTime; 
+        }
+    }
 
-
-
-
-
-
-
+    // FOV METHOD - Player detection mechanism
 
     private void FOV()
     {
--- src/Skeleton_game/Assets/Scripts/Final_Boss/boss_projectile.cs
+++ src/Skeleton_game/Assets/Scripts/Final_Boss/boss_projectile.cs
@@ -5,30 +5,23 @@ using UnityEngine;
 public class boss_projectile : MonoBehaviour
 {
     
+    // Projectile properties, changeable in Unity
     public int damage = 15;
     public float speed = 5;
-    private Vector3 target;
-    private Vector3 normDir;
-    private float maxDistance = 10f;
-    private Vector2 initialPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        var x = Random.Range(-20f, 20f); 
-        var y = Random.Range(-20f, 20f);
-
-        target = new Vector3(transform.position.x + x, transform.position.y + y, 0);
-        normDir = (target - transform.position).normalized;
-        initialPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += normDir * speed * Time.deltaTime;
+        
     }
 
+
+    // When proojectile collides with an object, it tries to deal damage to it and gets destroyed
     private void OnTriggerEnter2D(Collider2D other) {
         healthSystem opponent = other.GetComponent<healthSystem>();
         if (other.CompareTag("Player") || other.CompareTag("Boundary"))
